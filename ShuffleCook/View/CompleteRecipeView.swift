0874b7abaabf5777c021cb492e735e7e19dbc876
Nicolas Ebon <nicolasebon4@gmail.com>
2023-02-17 10:31:42 +1100

    CompleteRecipeView uses a parameterised recipe now

--- ShuffleCook/View/CompleteRecipeView.swift
+++ ShuffleCook/View/CompleteRecipeView.swift
@@ -5,13 +5,6 @@
 //  Created by Nicolas Ebon on 16/2/2023.
 //
 
-import SwiftUI
-
-struct CompleteRecipeView_Previews: PreviewProvider {
-    static var previews: some View {
-        CompleteRecipeView()
-    }
-}
 //
 //  RecipeView.swift
 //
@@ -25,29 +18,8 @@ import UIKit
 
 struct CompleteRecipeView: View {
     
+    var recipe:Recipe
     
-    private var recipe = Recipe(name: "Prawn & Kimchi Fried Rice",
-           ingredients: ["1 Lebanese cucumber, cut into matchsticks",
-                         "1 carrot, peeled, cut into matchsticks",
-                         "1 juiced lime",
-                         "2 corn cobs, Use a knife to cut down the side of corn to release the kernels",
-                        "2 tbs peanut oil",
-                        "20 Raw Banana Prawns, peeled leaving tails intact",
-                        "1 cup (250g) kimchi, save pickling liquid",
-                        "450g pkt microwavable jasmine rice",
-                        "2 Eggs, lightly whisked"],
-           instructions: ["1. Combine the cucumber, carrot and lime juice in a bowl. Season.",
-                          "2. Heat half the peanut oil in a wok or large non-stick frying pan over medium heat. Add the prawns and corn kernels and stir-fry for 3 mins or until prawns curl and change colour. Add the kimchi and stir-fry for 1 min or until heated through. Transfer the prawn mixture to a large bowl.",
-                          "3.Heat remaining oil in the wok or pan over high heat. Add the rice and stir-fry for 3 mins or until heated through. Add the kimchi liquid and stir-fry for 1 min or until liquid evaporates. Use a spatula to move rice to the edge of the wok or pan. Pour egg into centre of wok or pan and cook until just set. Break up egg and stir to combine with rice mixture. Return the prawn mixture to wok or pan and mix until well combined and heated through.",
-                         "4. Divide rice mixture among serving bowls. Top with cucumber mixture."],
-           totalTime: "15",
-           servings: 4,
-           cardColor: "Color2"
-          )
-    
-//    var recipe = Recipe(name:"testN", ingredients: ["2 eggs", "kimchi"]
-//    , instructions: ["1. 2. "], )
-//
     var body: some View {
         VStack{
             //page Title, add to list Hstack
@@ -137,4 +109,27 @@ struct CompleteRecipeView: View {
     }
 //View closed
     
+}
+
+struct CompleteRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteRecipeView(recipe: Recipe(name: "Prawn & Kimchi Fried Rice",
+                                          ingredients: ["1 Lebanese cucumber, cut into matchsticks",
+                                                        "1 carrot, peeled, cut into matchsticks",
+                                                        "1 juiced lime",
+                                                        "2 corn cobs, Use a knife to cut down the side of corn to release the kernels",
+                                                       "2 tbs peanut oil",
+                                                       "20 Raw Banana Prawns, peeled leaving tails intact",
+                                                       "1 cup (250g) kimchi, save pickling liquid",
+                                                       "450g pkt microwavable jasmine rice",
+                                                       "2 Eggs, lightly whisked"],
+                                          instructions: ["1. Combine the cucumber, carrot and lime juice in a bowl. Season.",
+                                                         "2. Heat half the peanut oil in a wok or large non-stick frying pan over medium heat. Add the prawns and corn kernels and stir-fry for 3 mins or until prawns curl and change colour. Add the kimchi and stir-fry for 1 min or until heated through. Transfer the prawn mixture to a large bowl.",
+                                                         "3.Heat remaining oil in the wok or pan over high heat. Add the rice and stir-fry for 3 mins or until heated through. Add the kimchi liquid and stir-fry for 1 min or until liquid evaporates. Use a spatula to move rice to the edge of the wok or pan. Pour egg into centre of wok or pan and cook until just set. Break up egg and stir to combine with rice mixture. Return the prawn mixture to wok or pan and mix until well combined and heated through.",
+                                                        "4. Divide rice mixture among serving bowls. Top with cucumber mixture."],
+                                          totalTime: "15",
+                                          servings: 4,
+                                          cardColor: "Color2"
+                                         ))
+    }
 }
