213b38fc9d94042b635cc3cecba68e3eb74f4354
Dixit-Tanna <dixittanna99@gmail.com>
2023-02-16 11:00:42 +1100

    Favourites button

--- ShuffleCook/View/BrowseRecipeView.swift
+++ ShuffleCook/View/BrowseRecipeView.swift
@@ -57,74 +57,85 @@ struct BrowseRecipeView: View {
         ZStack {
             NavigationView {
                 VStack (alignment: .leading){
+                    HStack {
                     Text("Browse")
                         .font(.largeTitle)
                         .padding(.top, 30)
                         .padding(.leading, 30)
                         .foregroundColor(Color("mainfont"))
+                 
+                     ZStack{
+                        
+                        
+                        NavigationLink(destination: ShoppingListView())
+                         {Image(systemName: "star.fill")
+                                 .padding(.top, 30)
+                                 .padding(.leading, 200)}
+                        }
+                    }
                     
-                    Divider()
-                        .frame(minWidth: 100)
-                        .frame(height: 1)
-                        .overlay(.gray.opacity(0))
-                    
-                    SearchBar(text: $text)
-                        .padding(.leading, 20)
-                        .padding(.trailing, 20)
-                    
-                    Divider()
-                        .frame(minWidth: 100)
-                        .frame(height: 1)
-                        .overlay(.gray.opacity(0))
-                    
-                    ScrollView(.vertical, showsIndicators: false){
-                        LazyVGrid(columns: columns, spacing: 20){
-                            ForEach(recipes.filter({ "\($0)".contains(text) || text.isEmpty})){ recipe in
-                                ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
-                                    NavigationLink(destination: ShoppingListView()){
-                                        VStack {
-                                            Image(recipe.name)
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame(height: 150)
-                                            
-                                            HStack {
-                                                VStack (alignment: .leading){
-                                                    Text(recipe.name)
-                                                        .font(.title3)
-                                                        .fontWeight(.bold)
-                                                        .foregroundColor(Color("mainfont"))
-                                                    //
-                                                    //                                            Text(recipe.totalTime)
-                                                    //                                                .foregroundColor(Color("subfont"))
-                                                    //
-                                                    //                                            Text(recipe.name)
-                                                    //                                                .font(.headline)
-                                                    //                                                .foregroundColor(Color("mainfont"))
-                                                    //                                                .fontWeight(.semibold)
-                                                }
+                        Divider()
+                            .frame(minWidth: 100)
+                            .frame(height: 1)
+                            .overlay(.gray.opacity(0))
+                        
+                        SearchBar(text: $text)
+                            .padding(.leading, 20)
+                            .padding(.trailing, 20)
+                        
+                        Divider()
+                            .frame(minWidth: 100)
+                            .frame(height: 1)
+                            .overlay(.gray.opacity(0))
+                        
+                        ScrollView(.vertical, showsIndicators: false){
+                            LazyVGrid(columns: columns, spacing: 20){
+                                ForEach(recipes.filter({ "\($0)".contains(text) || text.isEmpty})){ recipe in
+                                    ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
+                                        NavigationLink(destination: ShoppingListView()){
+                                            VStack {
+                                                Image(recipe.name)
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(height: 150)
                                                 
-                                                Spacer()
+                                                HStack {
+                                                    VStack (alignment: .leading){
+                                                        Text(recipe.name)
+                                                            .font(.title3)
+                                                            .fontWeight(.bold)
+                                                            .foregroundColor(Color("mainfont"))
+                                                        //
+                                                        //                                            Text(recipe.totalTime)
+                                                        //                                                .foregroundColor(Color("subfont"))
+                                                        //
+                                                        //                                            Text(recipe.name)
+                                                        //                                                .font(.headline)
+                                                        //                                                .foregroundColor(Color("mainfont"))
+                                                        //                                                .fontWeight(.semibold)
+                                                    }
+                                                    
+                                                    Spacer()
+                                                }
                                             }
                                         }
                                     }
+                                    .padding(.horizontal, 20)
+                                    .padding(.vertical, 20)
+                                    .background(Color(recipe.cardColor))
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                                    .shadow(color: Color(recipe.cardColor).opacity(0.5), radius: 10, x:0, y: 10)
                                 }
-                                .padding(.horizontal, 20)
-                                .padding(.vertical, 20)
-                                .background(Color(recipe.cardColor))
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                                .shadow(color: Color(recipe.cardColor).opacity(0.5), radius: 10, x:0, y: 10)
                             }
                         }
+                        .padding(.trailing, 20)
+                        .padding(.leading, 20)
+                        .padding(.top, 5)
                     }
-                    .padding(.trailing, 20)
-                    .padding(.leading, 20)
-                    .padding(.top, 5)
                 }
             }
         }
     }
-}
 
 struct BrowseRecipeView_Previews: PreviewProvider {
     static var previews: some View {
