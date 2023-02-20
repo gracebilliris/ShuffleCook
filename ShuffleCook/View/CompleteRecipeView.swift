8a54f02cd9d1c5e97b96a5e7f0529c7219c19302
John Collins <johnc1999@hotmail.com>
2023-02-20 11:48:05 +1100

    Start on adding global recipe list able to receive additions

--- ShuffleCook/View/CompleteRecipeView.swift
+++ ShuffleCook/View/CompleteRecipeView.swift
@@ -24,6 +24,7 @@ import UIKit
 
 
 struct CompleteRecipeView: View {
+    //@State public var selectedRecipes: [Recipe]
     
     private var recipe = Recipe(id: 7,
                                 name: "Prawn & Kimchi Fried Rice",
