a8806df8532ee2f509fc6cd2d5bc63f1d4628e70
gracebilliris <grace.billiris@gmail.com>
2023-02-23 13:00:07 +1100

    Fixing unit type

--- ShuffleCook/Model/Ingredient.swift
+++ ShuffleCook/Model/Ingredient.swift
@@ -35,10 +35,10 @@ struct Ingredient: Hashable, Codable { //, Identifiable
     func getQuantityStr() -> String {
         print(name + ": quantity " + String(quantity) + " truncate: " + String((quantity).truncatingRemainder(dividingBy: 10)) + ",\ntimes ten: " +  String((quantity*10).truncatingRemainder(dividingBy: 10)))
         if ((quantity*10).truncatingRemainder(dividingBy: 10) != 0.0 || (quantity*100).truncatingRemainder(dividingBy: 10) != 0.0){
-            return String(format: "%.2f", quantity)
+            return String(format: "%.2f", quantity) + " " + unitType
         }
         else{
-            return String(format: "%.0f", quantity)
+            return String(format: "%.0f", quantity) + " " + unitType
         }
     }
     
